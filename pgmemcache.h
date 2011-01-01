458b09ba659cd4e4973c49875c8a6405ac14d24d
Hannu Valtonen <hannu.valtonen@ormod.com>
2011-01-01 22:32:32 +0200

    Change copyright years for the year 2011.

--- pgmemcache.h
+++ pgmemcache.h
@@ -4,7 +4,7 @@
  * Copyright (c) 2004-2005 Sean Chittenden <sean@chittenden.org>
  * Copyright (c) 2007 Neil Conway <neilc@samurai.com>
  * Copyright (c) 2007 Open Technology Group, Inc. <http://www.otg-nc.com>
- * Copyright (c) 2008-2010 Hannu Valtonen <hannu.valtonen@hut.fi>
+ * Copyright (c) 2008-2011 Hannu Valtonen <hannu.valtonen@hut.fi>
  *
  * See the file COPYING for distribution terms.
  */
